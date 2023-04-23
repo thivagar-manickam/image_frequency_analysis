e20cbeea8606ce014a69fa45d3d8cd5016cb4817
thivagar-manickam <thivagar200@gmail.com>
2023-04-23 19:07:56 +0530

    Changes in the project description

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="image_frequency_analysis",
-    version="0.1",
+    version="0.2.0",
     author="M Thivagar",
     packages=find_packages(),
     install_requires=["opencv-python", "numpy", "pandas", "matplotlib"],
     python_requires=">=3.6",
+    homepage="https://github.com/thivagar-manickam/image_frequency_analysis",
+    description="A package to perform image frequency analysis using the Fourier Transform method",
+    bugtracker="https://github.com/thivagar-manickam/image_frequency_analysis/issues",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
